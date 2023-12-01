7ad8772a2b619fd724313300d53fa15fab8d7304
Omri Luzon <omril@testim.io>
2023-12-01 17:56:45 +0200

    remove part 2 commented code

--- day-1/part-2.ts
+++ day-1/part-2.ts
@@ -13,41 +13,20 @@ const digitNameToNumber = {
     nine: 'n9e',
 } as const;
 
-// const input = `two1nine
-// eightwothree
-// abcone2threexyz
-// xtwone3four
-// 4nineeightseven2
-// zoneight234
-// 7pqrstsixteen`;
 const input: string = fs.readFileSync('./day-1/input.txt', 'utf8');
 const lines = input.split('\n')//.slice(0, 30);
 const digits = lines.map(line => {
     let beforeSplit = line;
-    // const beforeSplit = line.replaceAll(
-    //     /(\d|one|two|three|four|five|six|seven|eight|nine)/g,
-    //     r => {
-    //         if (r in digitNameToNumber) {
-    //             return ` ${digitNameToNumber[r as keyof typeof digitNameToNumber]} `;
-    //         }
-    //         return r;
-    //     }
-    // );
     for (const digitName of Object.keys(digitNameToNumber)) {
         const regex = new RegExp(digitName, 'g');
         beforeSplit = beforeSplit.replace(regex, digitNameToNumber[digitName as keyof typeof digitNameToNumber]);
     }
-    // const beforeSplit = line.replaceAll(/(?=\d|one|two|three|four|five|six|seven|eight|nine)/g, r => ` ${r in digitNameToNumber ? digitNameToNumber[r as keyof typeof digitNameToNumber] : r} `);
     const afterSplit = beforeSplit.split(/(?!=\d)/);
     const afterFilter = afterSplit.filter(str => /\d/.test(str));
-    console.log({ line, beforeSplit, afterSplit, afterFilter });
     return afterFilter.map(str => Number(str));
 });
-// console.dir({ digits }, { depth: null });
 const firstAndLastTuple = digits.map(digit => [digit[0], digit.at(-1)!]);
-// console.log({ digits, firstAndLastTuple })
 const firstAndLastAsNumber = firstAndLastTuple.map(tuple => tuple[0] * 10 + tuple[1]);
 const sum = firstAndLastAsNumber.reduce((acc, curr) => acc + curr, 0);
 
-// 53885 is not correct for part 2
 console.log({ sum });
