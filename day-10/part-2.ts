2600a5895d3625339244c94883a59c70df3db5d5
Omri Luzon <omril@testim.io>
2023-12-11 22:22:22 +0200

    day 10 part 2

--- day-10/part-2.ts
+++ day-10/part-2.ts
@@ -26,8 +26,8 @@ L.L7LFJ|||||FJL7||LJ
 L7JLJL-JLJLJL--JLJ.L
 `.trim();
 
-const input = dummyInput;
-// const input: string = fs.readFileSync('./day-10/input.txt', 'utf8');
+// const input = dummyInput;
+const input: string = fs.readFileSync('./day-10/input.txt', 'utf8');
 console.log(input);
 
 // pipe maze
@@ -97,8 +97,8 @@ console.log(maze2.map((line) => line.join('')).join('\n'));
 console.log();
 
 function getNeighbors(maze: string[][], x: number, y: number) {
-    const width = lines[0].length;
-    const height = lines.length;
+    const width = maze[0].length;
+    const height = maze.length;
     let aboveNeighbor: { x: number; y: number; c: string } | undefined;
     let belowNeighbor: { x: number; y: number; c: string } | undefined;
     let leftNeighbor: { x: number; y: number; c: string } | undefined;
@@ -112,138 +112,143 @@ function getNeighbors(maze: string[][], x: number, y: number) {
     return { aboveNeighbor, belowNeighbor, leftNeighbor, rightNeighbor };
 }
 
-const visitedMazePipeLocations = new Set<`${number}-${number}`>();
-let currentX = startX;
-let currentY = startY;
-let stepNumber = 0;
+function getVisitedMazePipeLocationsInLoop(maze: string[][], startX: number, startY: number) {
+    const visitedMazePipeLocations = new Set<`${number}-${number}`>();
+    let currentX = startX;
+    let currentY = startY;
+    let stepNumber = 0;
 
-while (true) {
-    stepNumber++;
-    const currentChar = maze[currentY][currentX];
-    const { aboveNeighbor, belowNeighbor, leftNeighbor, rightNeighbor } = getNeighbors(maze, currentX, currentY);
-    // console.log({ currentX, currentY, currentChar, aboveNeighbor, belowNeighbor, leftNeighbor, rightNeighbor, stepNumber });
-
-    if (['S', '|', 'L', 'J'].includes(currentChar) && aboveNeighbor && ['|', '7', 'F'].includes(aboveNeighbor.c) && !visitedMazePipeLocations.has(`${aboveNeighbor.x}-${aboveNeighbor.y}`)) {
-        visitedMazePipeLocations.add(`${currentX}-${currentY}`);
-        currentX = aboveNeighbor.x;
-        currentY = aboveNeighbor.y;
-        continue;
-    }
-
-    if (['S', '|', '7', 'F'].includes(currentChar) && belowNeighbor && ['|', 'L', 'J'].includes(belowNeighbor.c) && !visitedMazePipeLocations.has(`${belowNeighbor.x}-${belowNeighbor.y}`)) {
-        visitedMazePipeLocations.add(`${currentX}-${currentY}`);
-        currentX = belowNeighbor.x;
-        currentY = belowNeighbor.y;
-        continue;
-    }
-
-    if (['S', '-', 'J', '7'].includes(currentChar) && leftNeighbor && ['-', 'L', 'F'].includes(leftNeighbor.c) && !visitedMazePipeLocations.has(`${leftNeighbor.x}-${leftNeighbor.y}`)) {
-        visitedMazePipeLocations.add(`${currentX}-${currentY}`);
-        currentX = leftNeighbor.x;
-        currentY = leftNeighbor.y;
-        continue;
-    }
-
-    if (['S', '-', 'L', 'F'].includes(currentChar) && rightNeighbor && ['-', 'J', '7'].includes(rightNeighbor.c) && !visitedMazePipeLocations.has(`${rightNeighbor.x}-${rightNeighbor.y}`)) {
-        visitedMazePipeLocations.add(`${currentX}-${currentY}`);
-        currentX = rightNeighbor.x;
-        currentY = rightNeighbor.y;
-        continue;
-    }
-
-    break;
-}
-visitedMazePipeLocations.add(`${currentX}-${currentY}`);
-
-console.log({ startX, startY, currentX, currentY, stepNumber, largestStepDistance: stepNumber / 2 });
-console.log();
-
-const visitedMazeGridLocations = new Set<`${number}-${number}`>();
-const nextToVisit: { x: number; y: number }[] = [
-    // { x: 0, y: 0 },
-    // { x: width - 1, y: height - 1 },
-];
-
-for (let x = 0; x < width; x++) {
-    if (!visitedMazePipeLocations.has(`${x}-${0}`)) {
-        nextToVisit.push({ x, y: 0 });
-    }
-    if (!visitedMazePipeLocations.has(`${x}-${height - 1}`)) {
-        nextToVisit.push({ x, y: height - 1 });
-    }
-}
-for (let y = 0; y < height; y++) {
-    if (!visitedMazePipeLocations.has(`${0}-${y}`)) {
-        nextToVisit.push({ x: 0, y: 0 });
-    }
-    if (!visitedMazePipeLocations.has(`${width - 1}-${y}`)) {
-        nextToVisit.push({ x: width - 1, y });
-    }
-}
-
-while (nextToVisit.length > 0) {
-    const { x, y } = nextToVisit.pop()!;
-    const { aboveNeighbor, belowNeighbor, leftNeighbor, rightNeighbor } = getNeighbors(maze2, x, y);
-
-    if (aboveNeighbor && !visitedMazeGridLocations.has(`${aboveNeighbor.x}-${aboveNeighbor.y}`) && !visitedMazePipeLocations.has(`${aboveNeighbor.x}-${aboveNeighbor.y}`)) {
-        nextToVisit.push(aboveNeighbor);
-    }
-
-    if (belowNeighbor && !visitedMazeGridLocations.has(`${belowNeighbor.x}-${belowNeighbor.y}`) && !visitedMazePipeLocations.has(`${belowNeighbor.x}-${belowNeighbor.y}`)) {
-        nextToVisit.push(belowNeighbor);
-    }
-
-    if (leftNeighbor && !visitedMazeGridLocations.has(`${leftNeighbor.x}-${leftNeighbor.y}`) && !visitedMazePipeLocations.has(`${leftNeighbor.x}-${leftNeighbor.y}`)) {
-        nextToVisit.push(leftNeighbor);
-    }
-
-    if (rightNeighbor && !visitedMazeGridLocations.has(`${rightNeighbor.x}-${rightNeighbor.y}`) && !visitedMazePipeLocations.has(`${rightNeighbor.x}-${rightNeighbor.y}`)) {
-        nextToVisit.push(rightNeighbor);
-    }
-    visitedMazeGridLocations.add(`${x}-${y}`);
-}
-
-for (let y = 0; y < height * 2; y++) {
-    for (let x = 0; x < width * 2; x++) {
-        if (visitedMazePipeLocations.has(`${x}-${y}`)) {
-            maze[y][x] = chalk.bgRed(maze[y][x]);
+    while (true) {
+        stepNumber++;
+        const currentChar = maze[currentY][currentX];
+        const { aboveNeighbor, belowNeighbor, leftNeighbor, rightNeighbor } = getNeighbors(maze, currentX, currentY);
+        //console.log({ currentX, currentY, currentChar, aboveNeighbor, belowNeighbor, leftNeighbor, rightNeighbor, stepNumber });
+        if (['S', '|', 'L', 'J'].includes(currentChar) && aboveNeighbor && ['|', '7', 'F'].includes(aboveNeighbor.c) && !visitedMazePipeLocations.has(`${aboveNeighbor.x}-${aboveNeighbor.y}`)) {
+            visitedMazePipeLocations.add(`${currentX}-${currentY}`);
+            currentX = aboveNeighbor.x;
+            currentY = aboveNeighbor.y;
+            continue;
         }
-        if (visitedMazeGridLocations.has(`${x}-${y}`)) {
-            maze[y][x] = chalk.bgGreen(maze[y][x]);
+
+        if (['S', '|', '7', 'F'].includes(currentChar) && belowNeighbor && ['|', 'L', 'J'].includes(belowNeighbor.c) && !visitedMazePipeLocations.has(`${belowNeighbor.x}-${belowNeighbor.y}`)) {
+            visitedMazePipeLocations.add(`${currentX}-${currentY}`);
+            currentX = belowNeighbor.x;
+            currentY = belowNeighbor.y;
+            continue;
         }
+
+        if (['S', '-', 'J', '7'].includes(currentChar) && leftNeighbor && ['-', 'L', 'F'].includes(leftNeighbor.c) && !visitedMazePipeLocations.has(`${leftNeighbor.x}-${leftNeighbor.y}`)) {
+            visitedMazePipeLocations.add(`${currentX}-${currentY}`);
+            currentX = leftNeighbor.x;
+            currentY = leftNeighbor.y;
+            continue;
+        }
+
+        if (['S', '-', 'L', 'F'].includes(currentChar) && rightNeighbor && ['-', 'J', '7'].includes(rightNeighbor.c) && !visitedMazePipeLocations.has(`${rightNeighbor.x}-${rightNeighbor.y}`)) {
+            visitedMazePipeLocations.add(`${currentX}-${currentY}`);
+            currentX = rightNeighbor.x;
+            currentY = rightNeighbor.y;
+            continue;
+        }
+
+        break;
     }
+    visitedMazePipeLocations.add(`${currentX}-${currentY}`);
+
+    console.log({ startX, startY, currentX, currentY, stepNumber, largestStepDistance: stepNumber / 2 });
+    console.log();
+    return visitedMazePipeLocations;
 }
 
-// print maze
-console.log(maze.map((line) => line.join('')).join('\n'));
-console.log();
+const visitedMazePipeLocations = getVisitedMazePipeLocationsInLoop(maze, startX, startY);
+const visitedMazePipeLocations2 = getVisitedMazePipeLocationsInLoop(maze2, startX * 2, startY * 2);
 
-for (let y = 0; y < height; y++) {
+console.log({ visitedMazePipeLocations: visitedMazePipeLocations.size, visitedMazePipeLocations2: visitedMazePipeLocations2.size });
+
+function getMazeBoundsLocations(visitedMazePipeLocations: Set<`${number}-${number}`>, width: number, height: number) {
+    const nextToVisit: { x: number; y: number; }[] = [];
+
     for (let x = 0; x < width; x++) {
-        const cacheKey = `${x}-${y}` as const;
-        if (visitedMazePipeLocations.has(cacheKey)) {
-            maze2[y * 2][x * 2] = chalk.bgRed(maze2[y * 2][x * 2]);
-            maze2[y * 2][x * 2 + 1] = chalk.bgRed(maze2[y * 2][x * 2 + 1]);
-            maze2[y * 2 + 1][x * 2] = chalk.bgRed(maze2[y * 2 + 1][x * 2]);
-            maze2[y * 2 + 1][x * 2 + 1] = chalk.bgRed(maze2[y * 2 + 1][x * 2 + 1]);
+        if (!visitedMazePipeLocations.has(`${x}-${0}`)) {
+            nextToVisit.push({ x, y: 0 });
         }
-        if (visitedMazeGridLocations.has(cacheKey)) {
-            maze2[y * 2][x * 2] = chalk.bgGreen(maze2[y * 2][x * 2]);
-            maze2[y * 2][x * 2 + 1] = chalk.bgGreen(maze2[y * 2][x * 2 + 1]);
-            maze2[y * 2 + 1][x * 2] = chalk.bgGreen(maze2[y * 2 + 1][x * 2]);
-            maze2[y * 2 + 1][x * 2 + 1] = chalk.bgGreen(maze2[y * 2 + 1][x * 2 + 1]);
+        if (!visitedMazePipeLocations.has(`${x}-${height - 1}`)) {
+            nextToVisit.push({ x, y: height - 1 });
         }
     }
+    for (let y = 0; y < height; y++) {
+        if (!visitedMazePipeLocations.has(`${0}-${y}`)) {
+            nextToVisit.push({ x: 0, y: 0 });
+        }
+        if (!visitedMazePipeLocations.has(`${width - 1}-${y}`)) {
+            nextToVisit.push({ x: width - 1, y });
+        }
+    }
+    return nextToVisit;
 }
 
-// print maze2
-console.log(maze2.map((line) => line.join('')).join('\n'));
-console.log();
+function getVisitedMazeGridLocations(maze: string[][], visitedMazePipeLocations: Set<`${number}-${number}`>) {
+    const nextToVisit: { x: number; y: number; }[] = getMazeBoundsLocations(visitedMazePipeLocations, maze[0].length, maze.length);
+    const visitedMazeGridLocations = new Set<`${number}-${number}`>();
 
-console.log({
-    visitedMazePipeLocations: visitedMazePipeLocations.size,
-    visitedMazeGridLocations: visitedMazeGridLocations.size,
-    mazeArea: width * height,
-    // 637 is too high
-    answer: (width * height) - visitedMazePipeLocations.size - visitedMazeGridLocations.size,
-});
+    while (nextToVisit.length > 0) {
+        const { x, y } = nextToVisit.pop()!;
+        const { aboveNeighbor, belowNeighbor, leftNeighbor, rightNeighbor } = getNeighbors(maze, x, y);
+
+        if (aboveNeighbor && !visitedMazeGridLocations.has(`${aboveNeighbor.x}-${aboveNeighbor.y}`) && !visitedMazePipeLocations.has(`${aboveNeighbor.x}-${aboveNeighbor.y}`)) {
+            nextToVisit.push(aboveNeighbor);
+        }
+
+        if (belowNeighbor && !visitedMazeGridLocations.has(`${belowNeighbor.x}-${belowNeighbor.y}`) && !visitedMazePipeLocations.has(`${belowNeighbor.x}-${belowNeighbor.y}`)) {
+            nextToVisit.push(belowNeighbor);
+        }
+
+        if (leftNeighbor && !visitedMazeGridLocations.has(`${leftNeighbor.x}-${leftNeighbor.y}`) && !visitedMazePipeLocations.has(`${leftNeighbor.x}-${leftNeighbor.y}`)) {
+            nextToVisit.push(leftNeighbor);
+        }
+
+        if (rightNeighbor && !visitedMazeGridLocations.has(`${rightNeighbor.x}-${rightNeighbor.y}`) && !visitedMazePipeLocations.has(`${rightNeighbor.x}-${rightNeighbor.y}`)) {
+            nextToVisit.push(rightNeighbor);
+        }
+        visitedMazeGridLocations.add(`${x}-${y}`);
+    }
+    return visitedMazeGridLocations;
+}
+
+const visitedMazeGridLocations = getVisitedMazeGridLocations(maze, visitedMazePipeLocations);
+const visitedMazeGridLocations2 = getVisitedMazeGridLocations(maze2, visitedMazePipeLocations2);
+
+function mutateAndPrintMazeColored(maze: string[][], visitedMazePipeLocations: Set<`${number}-${number}`>, visitedMazeGridLocations: Set<`${number}-${number}`>) {
+    for (let y = 0; y < maze.length; y++) {
+        for (let x = 0; x < maze[0].length; x++) {
+            if (visitedMazePipeLocations.has(`${x}-${y}`)) {
+                maze[y][x] = chalk.bgRed(maze[y][x]);
+            } else if (visitedMazeGridLocations.has(`${x}-${y}`)) {
+                maze[y][x] = chalk.bgGreen(maze[y][x]);
+            } else {
+                maze[y][x] = chalk.bgBlue(maze[y][x]);
+            }
+        }
+    }
+
+    // print maze
+    console.log(maze.map((line) => line.join('')).join('\n'));
+    console.log();
+}
+
+mutateAndPrintMazeColored(maze, visitedMazePipeLocations, visitedMazeGridLocations);
+mutateAndPrintMazeColored(maze2, visitedMazePipeLocations2, visitedMazeGridLocations2);
+
+function getPointsInsideWithEvenCoordinates(maze: string[][], visitedMazePipeLocations: Set<`${number}-${number}`>, visitedMazeGridLocations: Set<`${number}-${number}`>) {
+    let counter = 0;
+    for (let y = 0; y < maze.length; y++) {
+        for (let x = 0; x < maze[0].length; x++) {
+            if (!visitedMazePipeLocations.has(`${x}-${y}`) && !visitedMazeGridLocations.has(`${x}-${y}`) && (x % 2 === 0 && y % 2 === 0)) {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+
+console.log('getPointsInsideWithEvenCoordinates', getPointsInsideWithEvenCoordinates(maze2, visitedMazePipeLocations2, visitedMazeGridLocations2));
